81f2c35167854a8f92a73396c525621b29086f52
Lesyalys <nya.olesya@bk.ru>
2025-01-12 22:40:45 +0500

    new

---
+++ RefreshZapret/Form1.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.ServiceProcess;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RefreshZapret
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            InitTrayIconP();
+            InitializeComponent();
+
+        }
+
+        private void InitTrayIconP()
+        {
+            NotifyIcon notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.lovedsgn;
+            notifyIcon.Visible = true;
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+            ToolStripMenuItem toolStripMenuItem = new ToolStripMenuItem("Refresh Zapret");
+            toolStripMenuItem.Click += new EventHandler(RestartOtherApp_Click);
+
+            contextMenuStrip.Items.Add(toolStripMenuItem);
+
+
+        }
+
+        private void RestartOtherApp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("zapret"))
+                {
+ 
+                    proc.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+    }
+}
